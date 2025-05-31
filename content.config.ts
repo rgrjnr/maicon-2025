2abd529b04ad2c825664868ce9d32bf56c80ad72
Roger <roger@rogerjunior.com>
2025-05-31 08:25:21 +0100

    Initial config

---
+++ content.config.ts
@@ -0,0 +1,15 @@
+import { defineCollection, defineContentConfig, z } from '@nuxt/content'
+
+export default defineContentConfig({
+  collections: {
+    cases: defineCollection({
+      source: 'cases/*.md',
+      type: 'page',
+      // Define custom schema for docs collection
+      schema: z.object({
+        tags: z.array(z.string()),
+        title: z.string()
+      })
+    })
+  }
+})
